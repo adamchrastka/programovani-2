5e88ad53a43ac25d644f6b6b09365179cd6bdfb6
adamchrastka <adam.chratka@gmail.com>
2026-01-15 09:50:39 +0100

    zkouska

---
+++ 15.1.2026/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('vypisBtn'); 
+button.addEventListener('click', function() {
+    var retezec = document.getElementById("retezecInput").value;
+    let delka = retezec.length;
+    console.log(delka);
+});
